f4c2d193d6a19da46bd6ab2fb46805a88c319ae7
MhdAqif <mohammedaqif321@gmail.com>
2026-05-20 13:40:53 +0530

    Fix: ensure dotenv absolute path is loaded relative to BASE_DIR and set ALLOWED_HOSTS

--- solevault/settings.py
+++ solevault/settings.py
@@ -14,11 +14,11 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables relative to BASE_DIR
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
 
 
 # Application definition
